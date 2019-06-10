99d12fbc49167570075e305e0552e4bccfb6b457
Brendan Heinonen <invoke@platinumdigitalgroup.net>
2019-06-10 15:53:52 -0400

    added tests to public repo

---
+++ tests/agent.cpp
@@ -0,0 +1,72 @@
+#include "gtest/gtest.h"
+
+#include <libh6n/libh6n.h>
+
+class H6NSDKEnvironment : public testing::Environment {
+public:
+	void SetUp() override {
+		H6N_initialize();
+	}
+};
+
+::testing::Environment* const env = ::testing::AddGlobalTestEnvironment(new H6NSDKEnvironment);
+
+
+
+/*
+ * Regression testing for the SDK
+ */
+TEST(SDKAgent, TestNonExistingInterfaceName) {
+	EXPECT_EQ(Agent_createInterface("DoesNotExist", 1), nullptr);
+}
+
+TEST(SDKAgent, TestInvalidInterfaceName) {
+	EXPECT_EQ(Agent_createInterface("", 1), nullptr);
+}
+
+TEST(SDKAgent, TestNonExistingInterfaceVer) {
+	EXPECT_EQ(Agent_createInterface(H6AC_CLIENT_INTERFACE, 9001), nullptr);
+}
+
+TEST(SDKAgent, TestNegativeInterfaceVer) {
+	EXPECT_EQ(Agent_createInterface(H6AC_CLIENT_INTERFACE, -1), nullptr);
+}
+
+TEST(SDKAgent, TestClientCreateVer1) {
+	// Test creation
+	H6ACClient* cli = (H6ACClient*)Agent_createInterface(H6AC_CLIENT_INTERFACE, 1);
+	EXPECT_NE(cli, nullptr);
+
+	// Test that all calls don't crash
+	EXPECT_EQ(cli->isPlayerIDAquired(), 0);
+	H6N_Int128 id = { 0x1234, 0x1234 };
+	cli->setPlayerUniqueID(id);
+	cli->submitClientAttestation(nullptr, 0);
+	cli->purgeState();
+}
+
+TEST(SDKAgent, TestServerCreateVer1) {
+	// Test creation
+	H6ACServer* serv = (H6ACServer*)Agent_createInterface(H6AC_SERVER_INTERFACE, 1);
+	EXPECT_NE(serv, nullptr);
+
+	// Test that all calls don't crash
+	H6N_Int128 i;
+	serv->registerPlayerIPv4(i, 0, 0);
+	serv->registerPlayerIPv6(i, i, 0);
+	serv->setAttestationCallback(nullptr);
+	serv->setKickCallback(nullptr);
+	serv->unregisterPlayer(i);
+	serv->begin(i);
+	serv->end();
+	serv->setUpdateCallback(nullptr);
+}
+
+TEST(SDKAgent, TestClientAcquire) {
+	EXPECT_NE(Agent_createClient(), nullptr);
+}
+
+TEST(SDKAgent, TestServerAcquire) {
+	EXPECT_NE(Agent_createServer(), nullptr);
+}
+
